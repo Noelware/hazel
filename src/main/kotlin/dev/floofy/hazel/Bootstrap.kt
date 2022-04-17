8b6d3ada8f315b50d3bcd5031de4947c53e90f9c
Noel <cutie@floofy.dev>
2022-04-16 23:03:29 -0700

    chore: init spotless :feather:

--- src/main/kotlin/dev/floofy/hazel/Bootstrap.kt
+++ src/main/kotlin/dev/floofy/hazel/Bootstrap.kt
@@ -1,3 +1,20 @@
+/*
+ * 🪶 hazel: Minimal, simple, and open source content delivery network made in Kotlin
+ * Copyright 2022 Noel <cutie@floofy.dev>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package dev.floofy.hazel
 
 import gay.floof.utils.slf4j.logging
@@ -21,17 +38,19 @@ object Bootstrap {
 
     private fun installShutdownHook() {
         val runtime = Runtime.getRuntime()
-        runtime.addShutdownHook(thread(start = false, name = "Hazel-ShutdownThread") {
-            log.warn("Shutting down Hazel...")
+        runtime.addShutdownHook(
+            thread(start = false, name = "Hazel-ShutdownThread") {
+                log.warn("Shutting down Hazel...")
 
-            // Check if Koin has started
-            val koinStarted = GlobalContext.getKoinApplicationOrNull() != null
-            if (koinStarted) {
-                // TODO: this
+                // Check if Koin has started
+                val koinStarted = GlobalContext.getKoinApplicationOrNull() != null
+                if (koinStarted) {
+                    // TODO: this
+                }
+
+                log.warn("Hazel has completely shutdown, goodbye! ｡･ﾟﾟ･(థ Д థ。)･ﾟﾟ･｡")
             }
-
-            log.warn("Hazel has completely shutdown, goodbye! ｡･ﾟﾟ･(థ Д థ。)･ﾟﾟ･｡")
-        })
+        )
     }
 
     // credit: https://github.com/elastic/logstash/blob/main/logstash-core/src/main/java/org/logstash/Logstash.java#L98-L133
