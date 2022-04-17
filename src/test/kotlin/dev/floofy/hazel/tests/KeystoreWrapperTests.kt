faba320f327f848e3b37a0e744d26f1f3c2e158a
Noel <cutie@floofy.dev>
2022-04-17 14:10:36 -0700

    chore: spotless apply :sparkles:

--- src/test/kotlin/dev/floofy/hazel/tests/KeystoreWrapperTests.kt
+++ src/test/kotlin/dev/floofy/hazel/tests/KeystoreWrapperTests.kt
@@ -1,3 +1,20 @@
+/*
+ * 🪶 hazel: Minimal, simple, and open source content delivery network made in Kotlin
+ * Copyright 2022 Noel <cutie@floofy.dev>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 @file:Suppress("UNUSED")
 
 package dev.floofy.hazel.tests
@@ -12,10 +29,13 @@ import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 
 class KeystoreWrapperTests: DescribeSpec({
-    val keystore = KeystoreWrapper(KeystoreConfig(
-        file = "./owo.jks",
-        password = "owo da uwu"
-    ), Argon2Factory.create(), true)
+    val keystore = KeystoreWrapper(
+        KeystoreConfig(
+            file = "./owo.jks",
+            password = "owo da uwu"
+        ),
+        Argon2Factory.create(), true
+    )
 
     // The test order is so for each test that is executed, it will run
     // based off the order from the `describe` spec. If this wasn't here,
